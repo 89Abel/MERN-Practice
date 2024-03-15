11c8ce8d86a66f2839ce5fc199f853fc51336f72
unknown <abelgetahun18@gmail.com>
2024-03-15 10:02:41 -0700

    first commit

---
+++ Desktop/Bookstore-MERN-Stack/backend/config.js
@@ -0,0 +1,5 @@
+// Used this to create conncetions
+export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://abelgetahun18:wfxjaYFhMtYhdamg@books-store-mern.x2mljgl.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
